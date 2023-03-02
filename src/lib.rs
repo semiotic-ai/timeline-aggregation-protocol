710c6f78ee83743f03909ceffc8b14b8f6a98f44
Bryan Cole <bryan.cole@semiotic.ai>
2023-03-01 18:18:49 -0800

    test: Updated to rstest and added new cases
    Signed-off-by: Bryan Cole <bryan.cole@semiotic.ai>

--- src/lib.rs
+++ src/lib.rs
@@ -39,109 +39,140 @@ type Result<T> = std::result::Result<T, Error>;
 
 #[cfg(test)]
 mod tests {
+    use crate::{receipt::Receipt, receipt_aggregate_voucher::ReceiptAggregateVoucher, Error};
     use ethereum_types::Address;
-    use std::{
-        str::FromStr,
-        time::{SystemTime, UNIX_EPOCH},
-    };
-
     use k256::ecdsa::{SigningKey, VerifyingKey};
     use rand_core::OsRng;
+    use rstest::*;
+    use std::str::FromStr;
 
-    use crate::{receipt::Receipt, receipt_aggregate_voucher::ReceiptAggregateVoucher, Error};
-
-    #[test]
-    fn test_receipt() {
+    #[fixture]
+    fn keys() -> (SigningKey, VerifyingKey) {
         let signing_key = SigningKey::random(&mut OsRng);
+        let verifying_key = VerifyingKey::from(&signing_key);
+        (signing_key, verifying_key)
+    }
 
-        let allocation_id =
-            Address::from_str("0xabababababababababababababababababababab").unwrap();
-        let value = 10u64;
-        let timestamp_ns = u64::try_from(
-            SystemTime::now()
-                .duration_since(UNIX_EPOCH)
-                .unwrap()
-                .as_nanos(),
-        )
-        .unwrap();
-        let test_receipt = Receipt::new(allocation_id, timestamp_ns + 9, value, &signing_key);
+    #[fixture]
+    fn allocation_ids() -> Vec<Address> {
+        vec![
+            Address::from_str("0xabababababababababababababababababababab").unwrap(),
+            Address::from_str("0xdeaddeaddeaddeaddeaddeaddeaddeaddeaddead").unwrap(),
+            Address::from_str("0xbeefbeefbeefbeefbeefbeefbeefbeefbeefbeef").unwrap(),
+            Address::from_str("0x1234567890abcdef1234567890abcdef12345678").unwrap(),
+        ]
+    }
+
+    #[rstest]
+    #[case::basic_receipt_test(30, 20, 40, 100)]
+    #[case::closest_valid_min_timestamp(30, 30, 40, 100)]
+    #[case::closest_valid_max_timestamp(30, 20, 31, 100)]
+    #[case::closest_valid_min_max_timestamp(30, 30, 31, 100)]
+    #[case::timestamp_at_min(u64::MIN, u64::MIN, 31, 100)]
+    #[case::timestamp_at_max(u64::MAX-1, u64::MAX-1, u64::MAX, 100)]
+    #[case::min_value_amount(30, 20, 40, u64::MIN)]
+    #[case::min_value_amount(30, 20, 40, u64::MAX)]
+
+    fn receipt_is_valid(
+        keys: (SigningKey, VerifyingKey),
+        allocation_ids: Vec<Address>,
+        #[case] timestamp: u64,
+        #[case] timestamp_min: u64,
+        #[case] timestamp_max: u64,
+        #[case] value: u64,
+    ) {
+        let test_receipt = Receipt::new(allocation_ids[0], timestamp, value, &keys.0);
 
         assert!(test_receipt
             .is_valid(
-                VerifyingKey::from(signing_key),
-                &[allocation_id],
-                timestamp_ns - 10,
-                timestamp_ns + 10,
+                keys.1,
+                &[allocation_ids[0]],
+                timestamp_min,
+                timestamp_max,
                 None
             )
             .is_ok())
     }
 
-    #[test]
-    fn test_rav() {
-        let values = (0u64..2 ^ 20).collect::<Vec<_>>();
+    #[rstest]
+    #[case::basic_rav_test(vec![1,2,3,4], vec![45,56,34,23])]
+    #[case::rav_from_zero_valued_receipts(vec![1,2,3,4], vec![0,0,0,0])]
+    #[case::rav_with_same_timestamped_receipts(vec![1,1,1,1], vec![45,56,34,23])]
+    fn rav_is_valid_with_no_previous_rav(
+        keys: (SigningKey, VerifyingKey),
+        allocation_ids: Vec<Address>,
+        #[case] timestamps: Vec<u64>,
+        #[case] values: Vec<u64>,
+    ) {
+        // Create receipts
         let mut receipts = Vec::new();
-        let allocation_id =
-            Address::from_str("0xabababababababababababababababababababab").unwrap();
-        let signing_key = SigningKey::random(&mut OsRng);
-        let verifying_key = VerifyingKey::from(&signing_key);
-
-        for value in values {
-            let timestamp_ns = u64::try_from(
-                SystemTime::now()
-                    .duration_since(UNIX_EPOCH)
-                    .unwrap()
-                    .as_nanos(),
-            )
-            .unwrap();
-
-            receipts.push(Receipt::new(
-                allocation_id,
-                timestamp_ns,
-                value,
-                &signing_key,
-            ))
+        for (value, timestamp) in values.iter().zip(timestamps) {
+            receipts.push(Receipt::new(allocation_ids[0], timestamp, *value, &keys.0))
         }
 
-        let rav = ReceiptAggregateVoucher::aggregate_receipt(
-            &receipts,
-            verifying_key,
-            &signing_key,
+        let rav =
+            ReceiptAggregateVoucher::aggregate_receipt(&receipts, keys.1, &keys.0, None).unwrap();
+        assert!(rav.is_valid(keys.1, allocation_ids[0]).is_ok());
+    }
+
+    #[rstest]
+    #[case::basic_rav_test(vec![1,2,3,4], vec![45,56,34,23])]
+    #[case::rav_from_zero_valued_receipts(vec![1,2,3,4], vec![0,0,0,0])]
+    #[case::rav_with_same_timestamped_receipts(vec![1,1,2,2], vec![45,56,34,23])]
+    fn rav_is_valid_with_previous_rav(
+        keys: (SigningKey, VerifyingKey),
+        allocation_ids: Vec<Address>,
+        #[case] timestamps: Vec<u64>,
+        #[case] values: Vec<u64>,
+    ) {
+        // Create receipts
+        let mut receipts = Vec::new();
+        for (value, timestamp) in values.iter().zip(timestamps) {
+            receipts.push(Receipt::new(allocation_ids[0], timestamp, *value, &keys.0))
+        }
+
+        // Create previous RAV from first half of receipts
+        let prev_rav = ReceiptAggregateVoucher::aggregate_receipt(
+            &receipts[0..receipts.len() / 2],
+            keys.1,
+            &keys.0,
             None,
         )
         .unwrap();
-        assert!(rav.is_valid(verifying_key, allocation_id).is_ok());
+
+        // Create new RAV from last half of receipts and prev_rav
+        let rav = ReceiptAggregateVoucher::aggregate_receipt(
+            &receipts[receipts.len() / 2..receipts.len()],
+            keys.1,
+            &keys.0,
+            Some(prev_rav),
+        )
+        .unwrap();
+        assert!(rav.is_valid(keys.1, allocation_ids[0]).is_ok());
     }
 
-    #[test]
-    fn test_incorrect_allocation() {
-        let values = (0u64..2 ^ 20).collect::<Vec<_>>();
+    #[rstest]
+    #[case::basic_rav_test(vec![1,2,3,4], vec![45,56,34,23])]
+    #[case::rav_from_zero_valued_receipts(vec![1,2,3,4], vec![0,0,0,0])]
+    #[case::rav_with_same_timestamped_receipts(vec![1,1,1,1], vec![45,56,34,23])]
+    fn rav_with_invalid_receipt_allocation_id_submitted_errors(
+        keys: (SigningKey, VerifyingKey),
+        allocation_ids: Vec<Address>,
+        #[case] timestamps: Vec<u64>,
+        #[case] values: Vec<u64>,
+    ) {
+        // Create receipts
         let mut receipts = Vec::new();
-        let allocation_id =
-            Address::from_str("0xabababababababababababababababababababab").unwrap();
-        let false_allocation_id =
-            Address::from_str("0xcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd").unwrap();
-        let signing_key = SigningKey::random(&mut OsRng);
-        let verifying_key = VerifyingKey::from(&signing_key);
-
-        for value in values {
-            receipts.push(Receipt::new(allocation_id, value, value, &signing_key));
+        for (value, timestamp) in values.iter().zip(timestamps) {
+            receipts.push(Receipt::new(allocation_ids[0], timestamp, *value, &keys.0))
         }
 
-        // Add receipt with invalid allocation id
-        receipts.push(Receipt::new(false_allocation_id, 0u64, 0u64, &signing_key));
+        // Inject receipt with invalid allocation id
+        receipts.last_mut().unwrap().allocation_id = allocation_ids[1];
 
-        let false_rav = ReceiptAggregateVoucher::aggregate_receipt(
-            &receipts,
-            verifying_key,
-            &signing_key,
-            None,
-        )
-        .unwrap_err();
+        let false_rav =
+            ReceiptAggregateVoucher::aggregate_receipt(&receipts, keys.1, &keys.0, None)
+                .unwrap_err();
         assert!(matches!(false_rav, Error::InvalidAllocationID { .. }));
     }
 }
-
-// TODO: create test/documentation to ensure exclusivity in receipt validity check
-
-// TODO: Add test for timestamp "overlap" range, requestiing multiple RAVs and sending receipts that either should or shouldn't be accepted
