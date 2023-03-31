f5c37ffe63881b9e718964c060bdc4d1533e2e0e
Bryan Cole <bryan.cole@semiotic.ai>
2023-03-30 20:04:14 -0700

    docs(received_receipt): initial documentation for recieved receipt module
    Signed-off-by: Bryan Cole <bryan.cole@semiotic.ai>

--- src/tap_receipt/received_receipt.rs
+++ src/tap_receipt/received_receipt.rs
@@ -1,37 +1,67 @@
 // Copyright 2023-, Semiotic AI, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+//! Module containing Receipt with metadata for tracking receipt throught its lifecycle
+//!
+//! This module contains the `ReceivedReceipt` struct and associated enums `ReceiptState` and
+//! `RAVStatus`. The `ReceivedReceipt` struct is a wrapper class for a signed receipt, which
+//! includes metadata and state information for tracking the progress of a received receipt
+//! throughout its lifecycle. `ReceiptState` represents the different states a receipt can be in,
+//! while `RAVStatus` defines the status of a receipt with respect to its inclusion in RAV requests
+//! and received RAVs.
+//!
+//! This module is useful for managing and tracking the state of received receipts, as well as
+//! their progress through various checks and stages of inclusion in RAV requests and received RAVs.
+
 use super::{Receipt, ReceiptCheck, ReceiptCheckResults};
 use crate::eip_712_signed_message::EIP712SignedMessage;
 use strum_macros::{Display, EnumString};
 
 #[derive(Eq, PartialEq, Debug, Clone, EnumString, Display)]
+/// State of the contained receipt
 pub enum ReceiptState {
+    /// Initial state, received with no checks started
     Received,
+    /// Checking in progress, no errors found
     Checking,
+    /// At least one check resulted in an error
     Failed,
+    /// All checks have completed with no errors found
     Accepted,
+    /// Receipt was added to a RAV request
     IncludedInRAVRequest,
+    /// Receipt was included in received RAV
     Complete,
 }
 
 #[derive(Eq, PartialEq, Debug, Clone)]
 
+/// Status of receipt relating to RAV inclusion
 pub enum RAVStatus {
+    /// Has not been included in a RAV request or received RAV
     NotIncluded,
+    /// Has been added to a RAV request, but not a received RAV (awaiting a response)
     IncludedInRequest,
+    /// A RAV has been received that included this receipt
     IncludedInReceived,
 }
 
+/// Wrapper class for metadata and state of a received receipt
 pub struct ReceivedReceipt {
+    /// An EIP712 signed receipt message
     pub(crate) signed_receipt: EIP712SignedMessage<Receipt>,
+    /// A unique identifier for the query associated with the receipt
     pub(crate) query_id: u64,
+    /// A list of checks to be completed for the receipt, along with their current result
     pub(crate) checks: ReceiptCheckResults,
+    /// The current RAV status of the receipt (e.g., not included, included in a request, or included in a received RAV)
     pub(crate) rav_status: RAVStatus,
+    /// The current state of the receipt (e.g., received, checking, failed, accepted, etc.)
     pub(crate) state: ReceiptState,
 }
 
 impl ReceivedReceipt {
+    /// Initialize a new received receipt with provided signed receipt, query id, and checks
     pub fn new(
         signed_receipt: EIP712SignedMessage<Receipt>,
         query_id: u64,
@@ -48,11 +78,13 @@ impl ReceivedReceipt {
         received_receipt
     }
 
+    /// Update RAV status, should be called when receipt is included in RAV request and when RAV request is received
     pub fn update_rav_status(&mut self, rav_status: RAVStatus) {
         self.rav_status = rav_status;
         self.update_state();
     }
 
+    /// Update results of an incomplete receipt check, this is only valid if there are remaining checks to resolve
     pub fn update_check(
         &mut self,
         check: ReceiptCheck,
@@ -83,6 +115,7 @@ impl ReceivedReceipt {
         self.query_id
     }
 
+    /// Returns all checks that have not been completed
     pub fn incomplete_checks(&self) -> ReceiptCheckResults {
         self.checks
             .iter()
@@ -96,6 +129,7 @@ impl ReceivedReceipt {
             .collect()
     }
 
+    /// Returns all checks that completed with errors
     pub fn completed_checks_with_errors(&self) -> ReceiptCheckResults {
         self.checks
             .iter()
@@ -110,6 +144,7 @@ impl ReceivedReceipt {
             .collect()
     }
 
+    /// Updates receieved receipt state based on internal values, should be called anytime internal state changes
     fn update_state(&mut self) {
         let mut next_state = self.state.clone();
         match self.state {
