39c1c827447aceb938ad03643bb7bf08ff330cae
Bryan Cole <bryan.cole@semiotic.ai>
2023-04-25 09:32:58 -0700

    feat(adapters): split adapters into storage and check adapters
    Splits adapters by functionality instead of reseource requirement. The aim is to reduce the amount of adapters and improve interface consistency.
    
    BREAKING CHANGE: existing adapter trait definitions are updated
    
    Signed-off-by: Bryan Cole <bryan.cole@semiotic.ai>

--- tap_core/src/adapters/mod.rs
+++ tap_core/src/adapters/mod.rs
@@ -1,11 +1,13 @@
 pub mod allocation_adapter;
 pub mod collateral_adapter;
 pub mod rav_storage_adapter;
-pub mod receipt_adapter;
+pub mod receipt_checks_adapter;
+pub mod receipt_storage_adapter;
 
 mod test;
 
 pub use test::allocation_adapter_mock;
 pub use test::collateral_adapter_mock;
 pub use test::rav_storage_adapter_mock;
-pub use test::receipt_adapter_mock;
+pub use test::receipt_checks_adapter_mock;
+pub use test::receipt_storage_adapter_mock;
--- tap_core/src/adapters/test/mod.rs
+++ tap_core/src/adapters/test/mod.rs
@@ -1,9 +1,11 @@
 pub mod allocation_adapter_test;
 pub mod collateral_adapter_test;
 pub mod rav_storage_adapter_test;
-pub mod receipt_adapter_test;
+pub mod receipt_checks_adapter_test;
+pub mod receipt_storage_adapter_test;
 
 pub mod allocation_adapter_mock;
 pub mod collateral_adapter_mock;
 pub mod rav_storage_adapter_mock;
-pub mod receipt_adapter_mock;
+pub mod receipt_checks_adapter_mock;
+pub mod receipt_storage_adapter_mock;
