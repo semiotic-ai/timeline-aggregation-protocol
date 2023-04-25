957f3f99efa8b5ebe7f61024f96905b0448c4bed
Bryan Cole <bryan.cole@semiotic.ai>
2023-04-25 09:40:21 -0700

    fix(allocation-adapter): remove obsolete trait
    BREAKING CHANGE: removes allocation adapter
    
    Signed-off-by: Bryan Cole <bryan.cole@semiotic.ai>

--- tap_core/src/adapters/mod.rs
+++ tap_core/src/adapters/mod.rs
@@ -1,4 +1,3 @@
-pub mod allocation_adapter;
 pub mod collateral_adapter;
 pub mod rav_storage_adapter;
 pub mod receipt_checks_adapter;
@@ -6,7 +5,6 @@ pub mod receipt_storage_adapter;
 
 mod test;
 
-pub use test::allocation_adapter_mock;
 pub use test::collateral_adapter_mock;
 pub use test::rav_storage_adapter_mock;
 pub use test::receipt_checks_adapter_mock;
--- tap_core/src/adapters/test/mod.rs
+++ tap_core/src/adapters/test/mod.rs
@@ -1,10 +1,8 @@
-pub mod allocation_adapter_test;
 pub mod collateral_adapter_test;
 pub mod rav_storage_adapter_test;
 pub mod receipt_checks_adapter_test;
 pub mod receipt_storage_adapter_test;
 
-pub mod allocation_adapter_mock;
 pub mod collateral_adapter_mock;
 pub mod rav_storage_adapter_mock;
 pub mod receipt_checks_adapter_mock;
