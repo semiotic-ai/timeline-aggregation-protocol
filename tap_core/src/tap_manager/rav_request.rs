4c95b7ad844c2eb9da7b210ee9434fc125f63ffb
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2023-06-23 01:03:00 +0600

    refactor: Refactor error variants and usage of them
    Signed-off-by: Ozgur Akkurt <oezgurmakkurt@gmail.com>

--- tap_core/src/tap_manager/rav_request.rs
+++ tap_core/src/tap_manager/rav_request.rs
@@ -3,13 +3,14 @@
 
 use serde::{Deserialize, Serialize};
 
-use super::SignedReceipt;
+use super::{SignedRAV, SignedReceipt};
 use crate::receipt_aggregate_voucher::ReceiptAggregateVoucher;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 
 pub struct RAVRequest {
     pub valid_receipts: Vec<SignedReceipt>,
+    pub previous_rav: Option<SignedRAV>,
     pub invalid_receipts: Vec<SignedReceipt>,
     pub expected_rav: ReceiptAggregateVoucher,
 }
