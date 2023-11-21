5bb90015dc636aa4c0916e7072724ae7fc03e8e4
Alexis Asseman <alexis@semiotic.ai>
2023-11-20 17:18:52 -0800

    feat!: create_rav_request() returns invalid ReceivedReceipt
    Helps the user determine why the receipts failed, because
    ReceivedReceipt contains the status of checks.
    
    Signed-off-by: Alexis Asseman <alexis@semiotic.ai>

--- tap_core/src/tap_manager/rav_request.rs
+++ tap_core/src/tap_manager/rav_request.rs
@@ -4,13 +4,13 @@
 use serde::{Deserialize, Serialize};
 
 use super::{SignedRAV, SignedReceipt};
-use crate::receipt_aggregate_voucher::ReceiptAggregateVoucher;
+use crate::{receipt_aggregate_voucher::ReceiptAggregateVoucher, tap_receipt::ReceivedReceipt};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 
 pub struct RAVRequest {
     pub valid_receipts: Vec<SignedReceipt>,
     pub previous_rav: Option<SignedRAV>,
-    pub invalid_receipts: Vec<SignedReceipt>,
+    pub invalid_receipts: Vec<ReceivedReceipt>,
     pub expected_rav: ReceiptAggregateVoucher,
 }
