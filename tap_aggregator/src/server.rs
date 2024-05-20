14c91b133b5be11c23734fee206ddfcfba9b46a9
Joseph Livesey <joseph@semiotic.ai>
2024-05-20 19:17:19 -0400

    fix: run server on localhost to debug M3 networking issue

--- tap_aggregator/src/server.rs
+++ tap_aggregator/src/server.rs
@@ -224,7 +224,7 @@ pub async fn run_server(
         .max_response_body_size(max_response_body_size)
         .max_connections(max_concurrent_connections)
         .http_only()
-        .build(format!("0.0.0.0:{}", port))
+        .build(format!("127.0.0.1:{}", port))
         .await?;
     let addr = server.local_addr()?;
     println!("Listening on: {}", addr);
