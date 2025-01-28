7871bda22fbe1a04cb274e7f9cf09625d27116ee
Gustavo Inacio <gustavo@semiotic.ai>
2025-01-28 16:09:55 +0100

    docs: fix code examples
    Signed-off-by: Gustavo Inacio <gustavo@semiotic.ai>

--- tap_core/src/signed_message.rs
+++ tap_core/src/signed_message.rs
@@ -9,13 +9,14 @@
 //! # Example
 //! ```rust
 //! # use alloy::{dyn_abi::Eip712Domain, primitives::Address, signers::local::PrivateKeySigner};
+//! # use tap_graph::{Receipt};
 //! # let domain_separator = Eip712Domain::default();
-//! use tap_eip712_message::EIP712SignedMessage;
+//! use tap_core::signed_message::Eip712SignedMessage;
 //! # let wallet = PrivateKeySigner::random();
 //! # let wallet_address = wallet.address();
-//! # let message = msg::Receipt::new(Address::from([0x11u8; 20]), 100).unwrap();
+//! # let message = Receipt::new(Address::from([0x11u8; 20]), 100).unwrap();
 //!
-//! let signed_message = EIP712SignedMessage::new(&domain_separator, message, &wallet).unwrap();
+//! let signed_message = Eip712SignedMessage::new(&domain_separator, message, &wallet).unwrap();
 //! let signer = signed_message.recover_signer(&domain_separator).unwrap();
 //!
 //! assert_eq!(signer, wallet_address);
